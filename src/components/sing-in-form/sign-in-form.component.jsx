d8fb350b3b4e51cd5a3d4dbed02c26d016c72148
Amir Amiri <amiriamirr533@gmail.com>
2023-01-31 22:01:13 +0330

    auth component updated

--- src/components/sing-in-form/sign-in-form.component.jsx
+++ src/components/sing-in-form/sign-in-form.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -12,15 +13,25 @@ const defaultFormFields = {
 
 const SingInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields)
-    const {email, password } = formFields;
+    const { email, password } = formFields;
 
 
     const resetFormField = () => {
         setFormFields(defaultFormFields);
     }
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user)
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        try {
+            await signInAuthUserWithEmailAndPassword(email, password)
+            resetFormField();
+        } catch {
+            alert('email or password is incorrect.')
+        }
 
     };
 
@@ -31,13 +42,18 @@ const SingInForm = () => {
 
     return (
         <div className="sign-in-container">
+            <h2>Already have an account?</h2>
             <span>Sign in with email and password</span>
             <form onSubmit={handleSubmit}>
-                <FormInput label="Email" type='email' required onChange={handleChange} name="email" value={email}/>
-                <FormInput label="Password" type='password' required onChange={handleChange} name="password" value={password}/>
-                <Button buttonType='default' type="submit">Sign In</Button>
+                <FormInput label="Email" type='email' required onChange={handleChange} name="email" value={email} />
+                <FormInput label="Password" type='password' required onChange={handleChange} name="password" value={password} />
+                <div className="buttons-container">
+                    <Button buttonType='default' type="submit">Sign In</Button>
+                    <Button buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                </div>
             </form>
         </div>
+
     )
 }
 
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,22 +1,13 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 import SingUpForm from '../../components/sing-up-form/sign-up-form.component';
 import SingInForm from '../../components/sing-in-form/sign-in-form.component';
-import Button from '../../components/button/button.component';
-
-const SignIn = () => {
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user)
-    }
-
+import './authentiaction.styles.scss'
+const Authentication = () => {
     return (
-        <div>
-            <h1>Sign In Page</h1>
+        <div className='authentication-container'>
             <SingInForm/>
-            <Button buttonType='google' onClick={logGoogleUser}>sign in with google</Button>
             <SingUpForm/>
         </div>
     )
 }
 
-export default SignIn;
+export default Authentication;
